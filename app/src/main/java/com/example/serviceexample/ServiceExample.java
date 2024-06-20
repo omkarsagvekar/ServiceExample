5f1cb64bda94bc64d77c4f83f48046a2ba349089
Omkar <omkar.s@xploresense.com>
2024-06-20 12:02:31 +0530

    Added code for Intent service.

--- app/src/main/java/com/example/serviceexample/ServiceExample.java
+++ app/src/main/java/com/example/serviceexample/ServiceExample.java
@@ -1,5 +1,6 @@
 package com.example.serviceexample;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -7,7 +8,10 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
-public class ServiceExample extends Service {
+/****
+ * The below example is showing with standard Service.
+ */
+/*public class ServiceExample extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -34,4 +38,45 @@ public class ServiceExample extends Service {
         Log.d("Service", "Service is stopped");
     }
     
+}
+ */
+
+/****
+ * The below example is showing with Intent Service.
+ */
+public class ServiceExample extends IntentService {
+
+    /**
+     * @param name
+     * @deprecated
+     */
+    public ServiceExample() {
+        super("name");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("Service", "onHandleIntent method worked");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("Service", "Service is created");
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("Service", "Service is started");
+        //stopSelf(); //stop service if it was previously started.
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("Service", "Service is stopped");
+    }
+
 }
